e050c7355d00f6c3dfff17ea25e79feb253c4176
zkksch <safin008@yandex.ru>
2023-06-04 15:57:15 +0300

    Added description to the benchmark

--- tests/bench_test.go
+++ tests/bench_test.go
@@ -1,3 +1,41 @@
+/*
+Testing perfomance against other possible solutions for creating pipelines
+
+Test is pretty simple, we have slice of ints and pipeline that:
+
+	Removes ints that are not divisible by 2
+	Divides them by 2
+	Removes ints that are not divisible by 3
+	Divides them by 3
+	Counts remaining ints that are divisible by 5
+
+We're testing 4 possible solutions:
+
+	Fastest solution: hardcoded for loop, without pipeline
+	Slice solution: transforming slice into a new slice on each stage
+	Channel solution: using channels and goroutines to transport values
+	Iterator solution: using functionality of that library
+
+Here results of one of the runs on my PC:
+
+	BenchmarkVs/for_loop-1000-12         	  739795	      1465 ns/op	       0 B/op	       0 allocs/op
+	BenchmarkVs/slice_pipes-1000-12      	  107056	     10457 ns/op	   17776 B/op	      21 allocs/op
+	BenchmarkVs/chan_pipes-1000-12       	    1384	    901248 ns/op	     744 B/op	      10 allocs/op
+	BenchmarkVs/iter_pipes-1000-12       	   80746	     15336 ns/op	     144 B/op	       5 allocs/op
+	BenchmarkVs/for_loop-10000-12        	   21633	     55425 ns/op	       0 B/op	       0 allocs/op
+	BenchmarkVs/slice_pipes-10000-12     	    8270	    132290 ns/op	  222321 B/op	      31 allocs/op
+	BenchmarkVs/chan_pipes-10000-12      	     133	   8992839 ns/op	     803 B/op	      10 allocs/op
+	BenchmarkVs/iter_pipes-10000-12      	    6889	    180489 ns/op	     144 B/op	       5 allocs/op
+	BenchmarkVs/for_loop-100000-12       	    1916	    625871 ns/op	       0 B/op	       0 allocs/op
+	BenchmarkVs/slice_pipes-100000-12    	     759	   1617881 ns/op	 3156492 B/op	      48 allocs/op
+	BenchmarkVs/chan_pipes-100000-12     	      13	  89800554 ns/op	     760 B/op	      10 allocs/op
+	BenchmarkVs/iter_pipes-100000-12     	     727	   1667509 ns/op	     144 B/op	       5 allocs/op
+
+As we can see channel solution, that pretty often mentioned, is very slow, and slice solution allocates O(n) memory
+For loop is much faster but it doesn't provide flexibility of pipelines
+
+Iterators not that much slower than a slice solution, but have constant amount of memory usage and allocations (only need to allocate Iterators)
+*/
 package tests
 
 import (
@@ -5,9 +43,10 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/zkksch/iter/iter"
+	"github.com/zkksch/iter"
 )
 
+// The fastest solution
 func forLoop(s []int) int {
 	var cnt int
 	for _, el := range s {
@@ -26,6 +65,7 @@ func forLoop(s []int) int {
 	return cnt
 }
 
+// Filter pipe for a slice solution
 func sliceFilter[T any](s []T, fn func(T) bool) []T {
 	result := make([]T, 0)
 	for _, el := range s {
@@ -36,7 +76,10 @@ func sliceFilter[T any](s []T, fn func(T) bool) []T {
 	return result
 }
 
+// Map pipe for a slice solution
 func sliceMap[T, K any](s []T, fn func(T) (K, error)) ([]K, error) {
+	// We already know size of resulting slice,
+	// so we can allocate enough memory from the start
 	result := make([]K, 0, len(s))
 	for _, el := range s {
 		v, err := fn(el)
@@ -48,6 +91,7 @@ func sliceMap[T, K any](s []T, fn func(T) (K, error)) ([]K, error) {
 	return result, nil
 }
 
+// Count finalizer for a slice solution
 func sliceCnt[T any](s []T, fn func(T) bool) int {
 	var cnt int
 	for _, el := range s {
@@ -58,6 +102,7 @@ func sliceCnt[T any](s []T, fn func(T) bool) int {
 	return cnt
 }
 
+// Slice solution
 func slicePipe(s []int) (int, error) {
 	s = sliceFilter[int](s, func(i int) bool { return i%2 == 0 })
 	s, err := sliceMap[int, int](s, func(i int) (int, error) { return i / 2, nil })
@@ -73,6 +118,7 @@ func slicePipe(s []int) (int, error) {
 	return result, nil
 }
 
+// Creates a channel from a slice
 func sliceChan[T any](s []T) <-chan T {
 	result := make(chan T)
 	go func() {
@@ -84,6 +130,7 @@ func sliceChan[T any](s []T) <-chan T {
 	return result
 }
 
+// Filter pipe for a channel solution
 func filterChan[T any](c <-chan T, fn func(T) bool) <-chan T {
 	result := make(chan T)
 	go func() {
@@ -97,6 +144,7 @@ func filterChan[T any](c <-chan T, fn func(T) bool) <-chan T {
 	return result
 }
 
+// Map pipe for a channel solution
 func mapChan[T, K any](c <-chan T, fn func(T) (K, error)) <-chan K {
 	result := make(chan K)
 	go func() {
@@ -112,6 +160,7 @@ func mapChan[T, K any](c <-chan T, fn func(T) (K, error)) <-chan K {
 	return result
 }
 
+// Count finalizer for a channel solution
 func cntChan[T any](c <-chan T, fn func(T) bool) int {
 	var cnt int
 	for el := range c {
@@ -123,6 +172,7 @@ func cntChan[T any](c <-chan T, fn func(T) bool) int {
 	return cnt
 }
 
+// Channel solution
 func chanPipe(s []int) (int, error) {
 	sch := sliceChan[int](s)
 	sch = filterChan[int](sch, func(i int) bool { return i%2 == 0 })
@@ -132,12 +182,14 @@ func chanPipe(s []int) (int, error) {
 	return cntChan[int](sch, func(i int) bool { return i%5 == 0 }), nil
 }
 
+// Iterator solution
 func iterPipe(s []int) (int, error) {
 	sit := iter.FromSlice[int](s)
 	sit = iter.Filter[int](sit, func(i int) bool { return i%2 == 0 })
 	sit = iter.Map[int, int](sit, func(i int) (int, error) { return i / 2, nil })
 	sit = iter.Filter[int](sit, func(i int) bool { return i%3 == 0 })
 	sit = iter.Map[int, int](sit, func(i int) (int, error) { return i / 3, nil })
+	// Don't have count finalizer, but can use Reduce to achieve same result
 	result, err := iter.Reduce[int, int](sit, 0, func(el, acc int) int {
 		if el%5 == 0 {
 			acc += 1
@@ -147,6 +199,9 @@ func iterPipe(s []int) (int, error) {
 	return result, err
 }
 
+// Comparing solutions in 3 cases (N = 1'000, 10'000 and 100'000)
+// Generates slice with size N, fills it with random numbers (from 0 to N)
+// We also need to check that all of them done the same work by comparing results
 func BenchmarkVs(b *testing.B) {
 	cases := []int{
 		1000,
@@ -167,7 +222,7 @@ func BenchmarkVs(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				result := forLoop(data)
 				if result != valid {
-					b.Fatalf("wrong length of resulting array %v != %v", result, valid)
+					b.Fatalf("wrong result %v != %v", result, valid)
 				}
 			}
 		})
@@ -179,7 +234,7 @@ func BenchmarkVs(b *testing.B) {
 					b.Fatal(err)
 				}
 				if result != valid {
-					b.Fatalf("wrong length of resulting array %v != %v", result, valid)
+					b.Fatalf("wrong result %v != %v", result, valid)
 				}
 			}
 		})
@@ -191,7 +246,7 @@ func BenchmarkVs(b *testing.B) {
 					b.Fatal(err)
 				}
 				if result != valid {
-					b.Fatalf("wrong length of resulting array %v != %v", result, valid)
+					b.Fatalf("wrong result %v != %v", result, valid)
 				}
 			}
 		})
@@ -203,7 +258,7 @@ func BenchmarkVs(b *testing.B) {
 					b.Fatal(err)
 				}
 				if result != valid {
-					b.Fatalf("wrong length of resulting array %v != %v", result, valid)
+					b.Fatalf("wrong result %v != %v", result, valid)
 				}
 			}
 		})
